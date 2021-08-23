8f13f2864f7a629a0279f369c7253ccdb5a1c23a
Your Name <you@example.com>
2021-08-23 22:16:52 +0530

    Added: Move to cells based on index

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -19,15 +19,14 @@ public class TicTacToeGame {
 	  * turn=c means computer, turn=p means player 
 	  */
 	public static char turn; 
+	
+	//@parameter index to ask user where to mark X or O
+	public static int index;
 				
 	/*method for initializing the board
 	 * It will initialize the board values with space
-	 * Use case 1 :create a new board game and initialize the values to ' '
 	 */
-	
-	
 	public static void TicTacToeBoard() { 
-		char board[] = new char[10];
 		for (int i=1; i<board.length ; i++) {
 			board[i]= ' ';                      //Adding Space
 		}
@@ -69,15 +68,52 @@ public class TicTacToeGame {
 			}
 		}
 	}
+	
+	public static void desiredLocation()
+	{
+		
+		index=sc.nextInt();
+		if(index<1 || index>9)					//To check if the index is between 1 and 9
+		{
+			System.out.println("Please enter the index value between 1 and 9");
+			desiredLocation();
+		}
+		
+	}
+	
+	
+	/*@method moves to desired location
+	 * checks if the space is free
+	 * ability to make a move for desired location
+	 
+	 */
+	public static void desiredMove()
+	{
+		if(board[index]==' ')					//check if the index is free
+		{
+			board[index]=choice;
+		}
+		else
+		{
+			System.out.println("Current cell is already Filled. Please enter different index No.");
+			desiredLocation();					//@call desired location method
+			desiredMove();
+		}
+		showBoard();
+	}
 
 	//Main Function
 	public static void main(String[] args) {
 		System.out.println("Welcome"); //Welcome Message
 		TicTacToeGame obj = new TicTacToeGame();
 		obj.TicTacToeBoard(); //initializing the Board
-		turn='0';	//assuming that its players turn to play
+		turn='p';	//assuming that its players turn to play
 		inputChar();   //@method to enter input X or O
 		showBoard(); // @method to display entire board
+		System.out.println("Enter the index to move to desired cell");
+		
+		desiredLocation();          			//@method to get desired index from the player
+		desiredMove();     						//@method to move to different cells
 	}
 
 }
