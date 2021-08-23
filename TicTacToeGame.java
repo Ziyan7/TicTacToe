754163e657a3dd02256c2c711adfa18585404180
Your Name <you@example.com>
2021-08-23 19:45:45 +0530

    Added: Game Board Creation

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,10 +1,21 @@
 package TicTacToe;
 
+
 public class TicTacToeGame {
-
+	
+	public static void TicTacToeBoard() {
+		char board[] = new char[10];
+		for (int i=1; i<board.length ; i++) {
+			board[i]= ' ';                      //Adding Space
+		}
+	}
+	
+	//Main Function
 	public static void main(String[] args) {
-		System.out.println("Welcome");      //Welcome Message
-
+		System.out.println("Welcome");             //Welcome Message
+		
+		TicTacToeGame obj= new TicTacToeGame();    //object creation
+		obj.TicTacToeBoard();
 	}
 
 }
